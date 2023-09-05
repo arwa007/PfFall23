99875d6fec0bb699d68dbd063de91cfcd0db85cc
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-09-05 11:03:46 +0500

    Add files via upload

---
+++ labs/03/lab3q4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+            int orig_price=1500;
+            int sale_percentage= 0.8;
+            int final_price=0;
+            final_price= orig_price*sale_percentage;
+            printf("Final price of the pig is\n");
+            printf("%d", final_price);
+return 1;
+}
