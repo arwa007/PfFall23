f9ff1075bd7c5922fe2a356aae80e5d471c360cb
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-09-09 15:15:18 +0500

    Update lab3q3.c

--- labs/03/lab3q3.c
+++ labs/03/lab3q3.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
+include <stdio.h>
 #include <math.h>
 int main() {
-         int base=0;
-         int perp=0;
+         float base=0;
+         float perp=0;
          float hypsq=0;
          printf("Enter base length\n");
-         scanf("%d", &base);
+         scanf("%f", &base);
          printf("Enter perpendicular length\n");
-         scanf("%d", &perp);
+         scanf("%f", &perp);
          hypsq=base*base + perp*perp;
          printf("The hypotenuse is\n");
        printf("%f", sqrt(hypsq));
        return 1;
 }
-          
