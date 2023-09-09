6e25078a26fde26c5556422ecc6c2bfc1e74e58d
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-09-09 15:17:02 +0500

    Update lab3q4.c

--- labs/03/lab3q4.c
+++ labs/03/lab3q4.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main() {
-            int orig_price=1500;
-            int sale_percentage= 0.8;
-            int final_price=0;
+            float orig_price=1500;
+            float sale_percentage= 0.8;
+            float final_price=0;
             final_price= orig_price*sale_percentage;
             printf("Final price of the pig is\n");
-            printf("%d", final_price);
+            printf("%f", final_price);
 return 1;
 }
+      
