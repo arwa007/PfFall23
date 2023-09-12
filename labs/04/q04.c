bddeb4c324b1717f3ce756d87f7aae86bec2aed6
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-09-12 21:33:04 +0500

    Add files via upload

---
+++ labs/04/q04.c
@@ -0,0 +1,34 @@
+\\172.16.5.41\Exam Submission\Arwa Feroze
+/*
+ * Programmer: Arwa Feroze
+ * Date: 11/9/23
+ * Description: solve quadratic equations
+ * Related files:
+ */
+  //--Include Files--//
+ //--Global Varriables--//
+#include <stdio.h>
+#include<math.h>
+int main() {
+        float a=0;
+        float b=0;
+        float c=0;
+        float temp=0;
+        float temp2=0;
+        float x1=0;
+        float x2=0;
+        printf("Enter coefficient of x square (a) \n");
+        scanf("%f", &a);
+        printf("Enter coefficient of x (b) \n");
+        scanf("%f", &b);
+        printf("Enter constant (c) \n");
+        scanf("%f", &c);
+        temp=b*b-4*a*c;
+        temp2=-b+sqrt(temp);
+        x1=temp2/(2*a);
+        temp2=-b-sqrt(temp);
+        x2=temp2/(2*a);
+        printf("%f", x1);
+        printf(", ");
+        printf("%f", x2);
+        }
