ff2de38a5ebb01cd7615844b07c6ff7fc39dd9b6
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-09-12 10:49:22 +0500

    Update q01.c

--- labs/04/q01.c
+++ labs/04/q01.c
@@ -1,3 +1,15 @@
+\\172.16.5.41\Exam Submission\Arwa Feroze
+/*
+ * Programmer: Arwa Feroze
+ * Date: 11/9/23
+ * Description: Print a pattern by taking input n. Example n=3
+ ***
+ **
+ *
+ * Related files:
+ */
+  //--Include Files--//
+ //--Global Varriables--//
 #include<stdio.h>
 main() {
          int i=0;
