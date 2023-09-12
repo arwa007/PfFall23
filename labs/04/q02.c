2b6d724a275863b38810342540776e8470237cf5
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-09-12 10:50:08 +0500

    Update q02.c

--- labs/04/q02.c
+++ labs/04/q02.c
@@ -1,3 +1,12 @@
+\\172.16.5.41\Exam Submission\Arwa Feroze
+/*
+ * Programmer: Arwa Feroze
+ * Date: 11/9/23
+ * Description: Print n stars in n lines by taking input n.
+ * Related files:
+ */
+  //--Include Files--//
+ //--Global Varriables--//
 #include<stdio.h>
 main() {
          int i=0;
