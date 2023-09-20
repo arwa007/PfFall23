54525151554ed8bf222f5c53e2a6482c9c606040
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-09-20 22:12:39 +0500

    Update q05.c

--- labs/04/q05.c
+++ labs/04/q05.c
@@ -8,42 +8,50 @@
  */
   //--Include Files--//
  //--Global Varriables--//
+
 #include <stdio.h>
 int main()
 {
- int year1=0, year2=0, month1=0, month2=0, day1=0, day2=0;
+ int year1=0, year2=0, month1=0, month2=0, day1=0, day2=0, count=-1;
  printf("Enter person 1's date of birth in the following order: year, month, day.\n");
  scanf("%d%d%d", &year1, &month1, &day1);
  printf("Enter person 2's date of birth in the following order: year, month, day.\n");
- scanf("%d%d%d", &year2, &month2, &day2);\
- if (year2-year1<0) {
+ scanf("%d%d%d", &year2, &month2, &day2);
+ 
+while (year2-year1==0 && month2-month1==0 && day2-day1==0 && count<0){
+	printf("They are the same age.");
+	count=1;
+ 		   }  
+while (year2-year1>0 && count<0) {
  	printf("Person 2 is older.");
- }else{
-        if (year2-year1>0){
+ 	count=1;
+                                    }
+ while(year2-year1<0 && count<0){
             printf("Person 1 is older.");
-                            }
- 		   	}
- if (year2-year1 == 0){
-     if (month2-month1<0) {
+            count=1;
+                                 }
+ while (year2-year1 == 0 && count<0){
+     while (month2-month1<0 && count<0) {
  	printf("Person 2 is older.");
- }else{
-       if (year2-year1>0){
-        printf("Person 1 is older.");
- 		                    }
-			}
-                        }
-if (year2-year1==0 && month2-month1==0){
-	if (day2-day1<0) {
+ 	count=1;
+     }
+ 	while (month2-month1>0 && count<0) {
+ 	printf("Person 1 is older.");
+ 	count=1;
+     }
+     }                 
+while (year2-year1==0 && month2-month1==0 && count<0){
+	while (day2-day1<0 && count==0) {
  	printf("Person 2 is older.");
- }else{ 
+ 	count=1;
+ }
+ while (day2-day1>0 && count<0){ 
         if (day2-day1>0){
         printf("Person 1 is older.");
-                        }
+        count=1;
+		}
  }
-	  if (year2-year1==0 && month2-month1==0 && day2-day1==0){
-	printf("They are the same age.");
- 		   }  
-}
+}	  
 return 0;
 }
    
