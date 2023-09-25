5d5a2f8a95321bdfe6b58cfba840b90a8eec8cba
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-09-25 09:00:49 +0500

    Add files via upload

---
+++ labs/05/lab5q4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main (){
+float num1, num2, ans=0;
+char op;
+printf("Enter first number\n");
+scanf("%f", &num1);
+printf("Enter operator. Choose from +, -, *, and /.\n");
+scanf(" %c", &op);
+printf("Enter second number\n");
+scanf("%f", &num2);
+switch (op) {
+	case '+':
+	ans=num1+num2;
+	printf("Your answer is: %f", ans);
+	break;
+	case '-':
+	ans=num1-num2;
+	printf("Your answer is: %f", ans);
+	break;
+	case '*':
+	ans=num1*num2;
+	printf("Your answer is: %f", ans);
+	break;
+	case '/':
+	ans=num1/num2;
+	printf("Your answer is: %f", ans);
+	break;
+	default:
+printf("Oops..Try Again");
+break;
+}//end switch
+return 0;
+}//end main
