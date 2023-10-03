813e6d9ff3f54902061ac128a437b419c77b06e4
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-10-03 21:32:07 +0500

    Update lab5q1.c

--- labs/05/lab5q1.c
+++ labs/05/lab5q1.c
@@ -1,4 +1,11 @@
-#include <stdio.h>
+/*
+ * Programmer: Arwa Feroze
+ * Date: 18/9/23
+ * Description: Check if input is a multiple of 3.
+ * Related files:
+ */
+  //--Include Files--//
+ //--Global Varriables--//#include <stdio.h>#include <stdio.h>
 int main() {
 int num=0;
 printf("Enter number\n");
