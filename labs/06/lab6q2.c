b6c77ff28cb770b0b647961e236d2386aead3aee
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-10-03 09:53:27 +0500

    Add files via upload

---
+++ labs/06/lab6q2.c
@@ -0,0 +1,22 @@
+/*
+ * Programmer: Arwa Feroze
+ * Date: 3/10/23
+ * Description: Print Fibonacci series up to 10000. Also find the sum of the
+generated Fibonacci numbers divisible by 3, 5 or 7 only.
+ */
+  //--Include Files--//
+ //--Global Varriables--//
+#include <stdio.h>
+int main(){
+	int nextnum=0, prevnum = 1, count=1, temp=1, sum=0;
+	for(nextnum=0; nextnum<6000;){
+		temp= nextnum;
+		nextnum = nextnum + prevnum;
+		printf("%d, ", nextnum);
+		if(nextnum%3 == 0 || nextnum%5 == 0 || nextnum%7 == 0){
+		sum = sum + nextnum;
+		}//end if
+		prevnum = temp;
+}//end for
+printf("\n The sum the numbers in the fibionacci sequence that are divisible by 3, 5 or 7 is: %d", sum);
+}//end main
