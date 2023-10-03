ef0d6c24c4bd2353d58a2d5a9fb861e851d20988
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-10-03 11:07:25 +0500

    Add files via upload

---
+++ labs/06/lab6q4.c
@@ -0,0 +1,39 @@
+/*
+ * Programmer: Arwa Feroze
+ * Date: 3/10/23
+ * Description: Check if input is a perect number.
+ * Related files:
+ */
+  //--Include Files--//
+ //--Global Varriables--//
+#include <stdio.h>
+int main(){
+	printf("Enter two numbers.\n");
+	scanf("%d %d", &num1, &num2);
+	while(num1<= num2){
+	switch(num1){
+	case 1:
+		printf("One \n");
+	case 2:
+		printf("Two \n");
+ 	case 3:
+		printf("Three \n");
+	case 4:
+		printf("Four \n");
+	case 5:
+		printf("Five \n");
+	case 6:
+		printf("Six \n");
+	case 7:
+		printf("Seven \n");
+	case 8:
+		printf("Eight \n");
+	case 9:
+		printf("Nine \n");
+		
+	default:
+		
+	
+}//end while
+}//end for
+}// end main
