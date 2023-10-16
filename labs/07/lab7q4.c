438cb516ec3a08d24fc0992c5144e3b46df9d333
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-10-16 13:55:18 +0500

    Add files via upload

---
+++ labs/07/lab7q4.c
@@ -0,0 +1,35 @@
+/*
+Programmer: Arwa Feroze
+Date: 10/16/2023
+Desc: Practicing PF mid
+/*
+# include <stdio.h>
+# include <stdbool.h>
+int main(){
+	int i, j, size;
+	bool isSquare = true;
+	printf("Enter the size of your square matrix/array: ");
+	scanf("%d", &size);
+	int arr[size][size];
+	printf("Enter the Elements of your matrix/array: ");	
+	for (i=0; i < size; i++){
+		for (j=0; j < size; j++){
+			printf("Enter Element (%d, %d): ", i+1, j+1);
+			scanf("%d", &arr[i][j]);
+		}//end for
+	}//end for
+	for (i=2; i < size; i++){
+		for (j=2; j < size; j++){
+			if (arr[i][j] != arr[j][i]){
+				isSquare = false;
+			}//end if
+		}//end for
+	}//end for
+	if (isSquare){
+		printf("Array is symmetric.");
+	}else{
+		printf("Array is not symmetric.");
+	}//end if
+return 0;
+}//end main
+
