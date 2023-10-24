863202192ac31ac797a42c9c8a8d4cbb5ce0d834
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-10-24 09:42:10 +0500

    Add files via upload

---
+++ labs/08/lab8q2.c
@@ -0,0 +1,65 @@
+/*
+* Programmer: Arwa Feroze
+* Date: 24/10/2023
+* Desc: 
+*/
+/*
+ * Programmer: Arwa Feroze
+ * Date: 25/9/23
+ * Description: Calculator using Switch Case.
+ * Related files:
+ */
+#include <stdio.h>
+
+void addition(int num1, int num2){
+	float ans = 0;
+	ans=num1+num2;
+	printf("Your answer is: %f", ans);
+}//end addition 
+
+void substraction(int num1, int num2){
+	float ans = 0;
+	ans=num1-num2;
+	printf("Your answer is: %f", ans);
+}//end substraction
+
+void multiplication(int num1, int num2){
+	float ans = 0;
+	ans=num1*num2;
+	printf("Your answer is: %f", ans);
+}//end multiplication
+
+void division(int num1, int num2){
+	float ans = 0;
+	ans=num1/num2;
+	printf("Your answer is: %f", ans);
+}//end division
+
+int main (){
+float num1, num2;
+char op;
+printf("Enter first number\n");
+scanf("%f", &num1);
+printf("Enter operator. Choose from +, -, *, and /.\n");
+scanf(" %c", &op);
+printf("Enter second number\n");
+scanf("%f", &num2);
+switch (op) {
+	case '+':
+	addition(num1, num2);
+	break;
+	case '-':
+	substraction(num1, num2);
+	break;
+	case '*':
+	multiplication(num1, num2);
+	break;
+	case '/':
+	division(num1, num2);
+	break;
+	default:
+printf("Oops..Try Again");
+break;
+}//end switch
+return 0;
+}//end main
