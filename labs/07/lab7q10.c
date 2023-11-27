a12d1ea47488578f89dd185eaccd0b3e0dbca163
arwa007 <142319755+arwa007@users.noreply.github.com>
2023-11-27 16:34:37 +0500

    Add files via upload

--- labs/07/lab7q10.c
+++ labs/07/lab7q10.c
@@ -1,52 +1,43 @@
 /*
-*Programmer: Arwa Feroze
-*Date: 24/10/2023
-*Description: Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-
-array which adds to a given number S. In case of multiple subarrays, return the subarray which comes
-first on moving from left to right.
+* Programmer: Arwa Feroze
+* Date: 24/10/2023
+* Desc: ind a continuous sub-array in an unsorted array which adds to a given number S.
 */
-
 #include <stdio.h>
-#include <stdbool.h>
 
-int main() {
-    int S, size, i;
+void findSubarray(int arr[], int n, int target) {
+    int current_sum = 0, start = 0, end = 0;
 
-    printf("Enter the size of your array: ");
-    scanf("%d", &size);
+    while (end < n) {
+        current_sum += arr[end];
 
-    int arr[size];
-    
-    for (i = 0; i < size; i++) {
-        printf("Enter Element %d: ", i + 1);
-        scanf("%d", &arr[i]);
-    }
-
-    printf("Give a number S: ");
-    scanf("%d", &S);
-
-    int start = 0;
-    int sum = 0;
-    bool processSuccessful = false;
-
-    for (i = 0; i < size; i++) {
-        sum += arr[i];
-
-        while (sum > S) {
-            sum -= arr[start];
+        while (current_sum > target) {
+            current_sum -= arr[start];
             start++;
         }
 
-        if (sum == S) {
-            printf("The Elements from  %d to %d when summed result in the output of %d.\n", start + 1, i + 1, S);
-            processSuccessful = true;
-            break;
+        if (current_sum == target) {
+            printf("The elements from index %d to %d when summed result in %d.\n", start, end, target);
+            printf("Subarray: ");
+            for (int i = start; i <= end; i++) {
+                printf("%d ", arr[i]);
+            }
+            printf("\n");
+            return;
         }
+
+        end++;
     }
 
-    if (!processSuccessful) {
-        printf("No subarray found that adds up to %d.\n", S);
-    }
+    printf("No subarray found with the given sum.\n");
+}
+
+int main() {
+    int target_sum = 12;
+    int input_array[] = {1, 2, 3, 7, 5};
+    int size = sizeof(input_array) / sizeof(input_array[0]);
+
+    findSubarray(input_array, size, target_sum);
 
     return 0;
 }
--- labs/07/lab7q11.c
+++ labs/07/lab7q11.c
@@ -1,65 +1,43 @@
 /*
-*Programmer: Arwa Feroze
-*Date: 24/10/2023
-*Description: Find the largest value in every contiguous 3x3 matrix in the input grid.
-Return the generated matrix.
+* Programmer: Arwa Feroze
+* Date: 24/10/2023
+* Desc: Find the largest value in every contiguous 3x3 matrix in the grid.
 */
 #include <stdio.h>
 
-// Function to generate the maxLocal matrix
-void generateMaxLocal(int n, int grid[n][n], int maxLocal[n-2][n-2]) {
-    for (int i = 1; i < n - 1; ++i) {
-        for (int j = 1; j < n - 1; ++j) {
-            int maxVal = grid[i-1][j-1];
-            for (int x = i-1; x <= i+1; ++x) {
-                for (int y = j-1; y <= j+1; ++y) {
-                    if (grid[x][y] > maxVal) {
-                        maxVal = grid[x][y];
-                    }
-                }
-            }
-            maxLocal[i-1][j-1] = maxVal;
-        }
-    }
-}
+void findSubarray(int arr[], int n, int target) {
+    int current_sum = 0, start = 0, end = 0;
 
-// Function to print a matrix
-void printMatrix(int rows, int cols, int matrix[rows][cols]) {
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            printf("%d ", matrix[i][j]);
+    while (end < n) {
+        current_sum += arr[end];
+
+        while (current_sum > target) {
+            current_sum -= arr[start];
+            start++;
         }
-        printf("\n");
+
+        if (current_sum == target) {
+            printf("The elements from index %d to %d when summed result in %d.\n", start, end, target);
+            printf("Subarray: ");
+            for (int i = start; i <= end; i++) {
+                printf("%d ", arr[i]);
+            }
+            printf("\n");
+            return;
+        }
+
+        end++;
     }
+
+    printf("No subarray found with the given sum.\n");
 }
 
 int main() {
-    int n;
-    printf("Enter the size of the matrix (n x n): ");
-    scanf("%d", &n);
+    int target_sum = 12;
+    int input_array[] = {1, 2, 3, 7, 5};
+    int size = sizeof(input_array) / sizeof(input_array[0]);
 
-    int grid[n][n];
-    printf("Enter the elements of the matrix:\n");
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            scanf("%d", &grid[i][j]);
-        }
-    }
-
-    // Check if the matrix is at least 3x3
-    if (n < 3) {
-        printf("Error: The matrix should be at least 3x3.\n");
-        return 1;
-    }
-
-    int maxLocal[n-2][n-2];
-
-    // Generate the maxLocal matrix
-    generateMaxLocal(n, grid, maxLocal);
-
-    // Print the generated matrix
-    printf("\nGenerated maxLocal matrix:\n");
-    printMatrix(n-2, n-2, maxLocal);
+    findSubarray(input_array, size, target_sum);
 
     return 0;
 }
